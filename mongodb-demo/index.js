7434d49e5fa2cf2fee4ff07ed5de0a7f04188236
cadolphe <christopher.adolphe@gmail.com>
2020-11-18 21:06:59 +0400

    Querying documents

--- mongodb-demo/index.js
+++ mongodb-demo/index.js
@@ -2,9 +2,12 @@
 const mongoose = require('mongoose');
 
 /**
+ * MongoDB is a document database. Its stores JSON objects.
  * A collection in MongoDB is similar to a table in the relational database context.
  * A document in MongoDB is similar to a row in the relational database context. Each document
  * is a container of key value pairs.
+ * 
+ * Mongoose is a schema-based solution to model application data.
  * A schema in Mongoose is used to define the properties of a document in MongoDB.
 */
 
@@ -28,15 +31,84 @@ const Course = mongoose.model('Course', courseSchema);
 const createCourse = async () => {
   // Instantiating a course object from the Course class
   const course = new Course({
-    name: 'Angular - The Complete Guide',
+    name: 'Vue.js - The Complete Guide',
     author: 'Maximilian Schwarzmuller',
     tags: ['javascript', 'front-end'],
     isPublished: true
   });
 
   // Saving the document to the collection
-  const result = await course.save();
-  console.log(result);
+  try {
+    const result = await course.save();
+    console.log(result);
+  } catch(error) {
+    console.error('Sorry could not create course', error);
+  }
 };
 
-createCourse();
+// createCourse();
+
+const getCourses = async () => {
+  /**
+   * MongoDB comparison operators
+   * 
+   * eq - equal
+   * ne - not equal
+   * gt - greater than
+   * gte - greater than or equal to
+   * lt- less than
+   * lte less than or equal to
+   * in
+   * nin - not in
+   * 
+   * e.g: Course.find({price: {$gte: 10, $lte: 20}})
+   * e.g: Course.find({price: {$in: [10, 20, 30]}})
+  */
+
+  /**
+   * MongoDB logical operators
+   * 
+   * or
+   * and
+   * 
+   * e.g: Course.find().or([{author: 'Max'}, {isPublished: true}])
+   * e.g: Course.find().and([{author: 'Max'}, {isPublished: true}])
+  */
+
+  /**
+   * Query filter with regular expression
+   * 
+   * Starts with John
+   * e.g Course.find({author: /^John/})
+   * 
+   * Ends with Doe and case insensitive
+   * e.g Course.find({author: /Doe$/i})
+   */
+
+  // Contains John
+  // e.g Course.find({author: /.*John.*/i})
+
+  /**
+   * Pagination
+   * 
+   * const pageNumber = 2;
+   * const pageSize = 10;
+   * 
+   * Course.find().skip((pageNumber - 1) * pageSize).limit(pageSize)
+  */
+
+  // Fetching the courses documents
+  try {
+    const courses = await Course
+      .find()
+      .or([{author: 'Mosh Hamedani'}])
+      .limit(10)
+      .sort({name: -1})
+      .select({name: 1, tags: 1})
+    console.log(courses);
+  } catch(error) {
+    console.error('Sorry could not fetch courses', error);
+  }
+};
+
+getCourses();
