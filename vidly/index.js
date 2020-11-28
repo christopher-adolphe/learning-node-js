3b526d99d6f0f7d306792a38c6a4b9d8a141738a
cadolphe <christopher.adolphe@gmail.com>
2020-11-28 10:18:08 +0400

    Added mongoose to dependencies and moved addition of middleware before definition of routes

--- vidly/index.js
+++ vidly/index.js
@@ -4,21 +4,19 @@ const express = require('express');
 const rootRouter = require('./routes/root');
 // Loading the Genres module
 const genresRouter = require('./routes/genres');
-// Loading Joi module
-const Joi = require('joi');
 
 // Creating an Express application
 const app = express();
-
-// Defining endpoint routes
-app.use('/', rootRouter);
-app.use('/api/genres', genresRouter);
 
 // Defining a port number for the Express application
 const port = process.env.PORT || 3000;
 
 // Adding Json middleware to convert request body into Json
 app.use(express.json());
+
+// Defining routes
+app.use('/', rootRouter);
+app.use('/api/genres', genresRouter);
 
 app.listen(port, () => {
   console.log(`Server is up and running on ${port}... Click here http://localhost:${port}`);
