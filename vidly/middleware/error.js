37302ea9d8f2bde440b703e949f6e98c9e84c970
cadolphe <christopher.adolphe@gmail.com>
2021-01-10 11:54:01 +0400

    Added asyncMiddleware and refactored route handlers to remove try catch blocks

--- vidly/middleware/error.js
+++ vidly/middleware/error.js
@@ -1,5 +1,5 @@
 const errorHandler = (error, request, response, next) => {
-  response.status(500).send(`Sorry, something went wrong: ${exception.message}`);
+  response.status(500).send(`Sorry, something went wrong: ${error.message}`);
 };
 
 module.exports = errorHandler;
